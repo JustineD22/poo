4124785505327a1dc63b71d3ea0bf6e7f8e82c78
Justine <98328323+JustineD22@users.noreply.github.com>
2022-02-28 12:44:36 +0100

    class sql

--- classes/Sql.php
+++ classes/Sql.php
@@ -8,5 +8,21 @@ class Sql
     private string $userPassword = "";
     private object $connexion;
 
-    
+    public function __construct()
+    {
+        try {
+            $this->connexion = new PDO("mysql:host$this->serverName;
+            dbname = $this->database", $this->userName, $this->userPassword);
+            $this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+
+        catch(PDOException $e) {
+            die("Erreur :" . $e->getMessage());
+        }
+    }
+
+    public function __destruct()
+    {
+        unset($this->connexion);
+    }
 }
