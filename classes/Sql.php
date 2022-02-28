2bb5d008000a5ffa0ba99a67d363b936fec647bd
Justine <98328323+JustineD22@users.noreply.github.com>
2022-02-28 12:28:52 +0100

    index et classe sql

---
+++ classes/Sql.php
@@ -0,0 +1,12 @@
+<?php
+
+class Sql
+{
+    private string $serverName = "localhost";
+    private string $userName = "root";
+    private string $database = "poo";
+    private string $userPassword = "";
+    private object $connexion;
+
+    
+}
