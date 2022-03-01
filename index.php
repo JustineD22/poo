b8f9f092ad798e1e46ed197d8f67736d64f1ba34
Justine <98328323+JustineD22@users.noreply.github.com>
2022-03-01 11:30:45 +0100

    commentaire du test

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ spl_autoload_register(function($className){
     include './classes/' . $className . '.php';
 });
 
-$test = new User();
-$test->setUserName("Donnadieu");
-$test->setUserFirstame("Justine");
-$test->setUserEmail("juju.donnadieu@hotmail.fr");
-$test->setBirthDate("2002-03-22");
+// $test = new User();
+// $test->setUserName("Donnadieu");
+// $test->setUserFirstame("Justine");
+// $test->setUserEmail("juju.donnadieu@hotmail.fr");
+// $test->setBirthDate("2002-03-22");
 
-$connexion = new Sql();
+// $connexion = new Sql();
 
-var_dump($test->inscription($connexion));
+// var_dump($test->inscription($connexion));
